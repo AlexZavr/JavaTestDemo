977bf503d9c86903e1ad1b8e7be3d9f44dcb21eb
AlexZavr <alex.zavr.27100@mail.ru>
2019-10-01 21:23:04 +0400

    AddBeforEachAndAfterEach

--- src/com/javatestdemo/BinarySearchTest.java
+++ src/com/javatestdemo/BinarySearchTest.java
@@ -1,29 +1,27 @@
 package com.javatestdemo;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
-@RunWith(JUnit4.class)
 public class BinarySearchTest {
 
     private static double[] arr;
 
-    @Before
-    public void setArray() {
+    @BeforeEach
+    void setUp() {
         arr = new double[] {0, 1, 2, 3, 4, 5};
     }
 
-    @After
-    public void clearArray() {
+    @AfterEach
+    void tearDown() {
         arr = new double[] {};
     }
 
-    @org.junit.Test
-    public void testFind() {
+    @Test
+    void find() {
         assertEquals(-1, BinarySearch.find(-5, arr));
         assertEquals(3, BinarySearch.find(3, arr));
     }
